84c18cfcb7625f142ae8dd58e9e002bd3e3b4a08
sinner <1416411712@qq.com>
2026-05-19 12:39:34 +0800

    leetcode_128

---
+++ hash/leetcode_128.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        st = set(nums)
+        ans = 0
+        for x in st:
+            if x - 1 in st:
+                continue
+            y = x + 1
+            while y in st:
+                y += 1
+            ans = max(ans, y - x)
+        return ans
